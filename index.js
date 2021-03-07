48f08adcd9a4822d525d1ce7479ceb6ad253e7bb
mitchell glass <mitchellglss@gmail.com>
2021-03-07 13:47:55 -0600

    Update mobile menu

--- index.js
+++ index.js
@@ -4,6 +4,16 @@
 // is static (always at top of the page)
 
 function closeMenu() {
-	console.log("test");
-	document.getElementById("toggle").click();
+	document.getElementById("toggle").click(); // click the "toggle" element which = hamburger menu which closes menu
+
+	// scroll page down by 75 pixels to account for header
+	// unless page is greater than 1500 pixels
+	// because then fullpage.JS is activated which accounts
+	// for the header when it auto-scrolls
+	console.log(window.innerWidth);
+	if (window.innerWidth < 1500) {
+		setTimeout(function () {
+			window.scrollBy(0, -75);
+		}, 2);
+	}
 }
