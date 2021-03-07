4a93c2fd921def0b1ffe734d6c3d39787cce642c
mitchell glass <mitchellglss@gmail.com>
2021-03-07 13:37:32 -0600

    Add Mobile Menu

---
+++ index.js
@@ -0,0 +1,9 @@
+// Close out of the menu after user clicks on an anchor
+// This is only relevant on mobile view (the lowest width
+// media query), because on all other views the menu
+// is static (always at top of the page)
+
+function closeMenu() {
+	console.log("test");
+	document.getElementById("toggle").click();
+}
